b86c6f0234901faf0d62c7399261e2eb908a6f1d
Alexandra Saganelidze <alekarah.all@gmail.com>
2025-04-03 19:29:10 +0300

    first commit

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-
 	_ "modernc.org/sqlite"
 )
 
@@ -21,7 +21,31 @@ func (s Sale) String() string {
 func selectSales(client int) ([]Sale, error) {
 	var sales []Sale
 
-	// напишите код здесь
+	db, err := sql.Open("sqlite", "demo.db")
+	if err != nil {
+        return nil, fmt.Errorf("ошибка подключения к БД: %w", err)
+    }
+    defer db.Close()
+
+	query := "SELECT product, volume, date FROM sales WHERE client = ?"
+	rows, err := db.Query(query, client)
+    if err != nil {
+        return nil, fmt.Errorf("ошибка выполнения запроса: %w", err)
+    }
+    defer rows.Close()
+
+	for rows.Next() {
+		var sale Sale
+		err := rows.Scan(&sale.Product, &sale.Volume, &sale.Date)
+		if err != nil {
+            return nil, fmt.Errorf("ошибка при сканировании строки: %w", err)
+        }
+		sales = append(sales, sale)
+	}
+
+	if err := rows.Err(); err != nil {
+        return nil, fmt.Errorf("ошибка при обработке результатов: %w", err)
+    }
 
 	return sales, nil
 }
